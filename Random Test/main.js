2e4156655e4364c186603fbe44881ab362035352
3tch-a-sketch <sametchells@icloud.com>
2019-05-02 19:41:50 +0100

    gen rand

--- Random Test/main.js
+++ Random Test/main.js
@@ -1,6 +1,11 @@
 const max = "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"
 const min = "0"
 
+data = [];
+var dataSRC = "";
 
-var chars = "1"
-var rand = Math.round(Math.random() * (max - min) + min);
+for (i = 0; i < 100; i++){
+   dataSRC += Math.round(Math.random() * (9 - 0) + 0);
+}
+
+console.log(dataSRC);
